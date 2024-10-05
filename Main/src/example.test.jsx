5e3ff861ab98c065e2ebec8de7b15b005b28234b
CaydenAnd <cand164@lsu.edu>
2024-10-04 19:43:29 -0500

    Skeleton

---
+++ Main/src/example.test.jsx
@@ -0,0 +1,8 @@
+import {describe, it, expect} from "vitest";
+
+describe("Example Suite", () => {
+    it("should pass"), () => {
+        const sum = 2 + 2
+        expect(sum).toEqual(4)
+    }
+})
