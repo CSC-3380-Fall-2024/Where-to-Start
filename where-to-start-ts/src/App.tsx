875c39d29e315e647390f0d6c4c0cbda8c016dbd
Jdav331 <Jdav331@lsu.edu>
2024-11-02 21:31:40 -0500

    Added landing page and formatting for it. 1 usable button

--- where-to-start-ts/src/App.tsx
+++ where-to-start-ts/src/App.tsx
@@ -2,6 +2,9 @@
     Where the code is done for many entry points, pages,
     in this application
 */
+import React from 'react';
+import Contact from './Contact/Contact';
+
 function App() {
 
   return (
--- where-to-start-ts/src/main.tsx
+++ where-to-start-ts/src/main.tsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import LandingPage from './Landing/LandingPage';
+import Contact from './Contact/Contact';
 
-//Entry Point To The Whole Application
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>,
+  document.getElementById('root')
+);
